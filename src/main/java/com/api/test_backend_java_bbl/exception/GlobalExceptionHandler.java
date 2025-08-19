2026d6b46693b3026d7d4ba9087e39ca51427b8f
Wirapat Prigphet <D6600531@outsource.ktb.co.th>
2025-08-19 23:26:11 +0700

    feat: add exeption handle required field

--- src/main/java/com/api/test_backend_java_bbl/exception/GlobalExceptionHandler.java
+++ src/main/java/com/api/test_backend_java_bbl/exception/GlobalExceptionHandler.java
@@ -1,11 +1,15 @@
 package com.api.test_backend_java_bbl.exception;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
+
+import java.util.List;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -22,5 +26,16 @@ public class GlobalExceptionHandler {
     public ResponseEntity<?> handleDuplicateUserException(DuplicateUserException exception) {
         return ResponseEntity.status(HttpStatus.CONFLICT)
                 .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(value = { MethodArgumentNotValidException.class })
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        List<String> defaultErrorMsg = exception.getBindingResult().getFieldErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .toList();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(defaultErrorMsg);
     }
 }
