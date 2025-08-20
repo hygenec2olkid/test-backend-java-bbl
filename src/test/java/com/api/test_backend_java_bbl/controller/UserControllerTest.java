c0b51b6b06027e6c511a842180e44b8b2db76b2c
Wirapat Prigphet <D6600531@outsource.ktb.co.th>
2025-08-20 23:29:42 +0700

    style: remove import not use

--- src/test/java/com/api/test_backend_java_bbl/controller/UserControllerTest.java
+++ src/test/java/com/api/test_backend_java_bbl/controller/UserControllerTest.java
@@ -1,6 +1,5 @@
 package com.api.test_backend_java_bbl.controller;
 
-import com.api.test_backend_java_bbl.model.CreateNewUserRequest;
 import com.api.test_backend_java_bbl.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
