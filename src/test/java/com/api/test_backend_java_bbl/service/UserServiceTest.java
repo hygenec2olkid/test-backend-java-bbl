0d7cb9f1e1e2d6dab2ecd1db62f8a4120698ddb7
Wirapat Prigphet <D6600531@outsource.ktb.co.th>
2025-08-20 22:52:21 +0700

    feat: add unit test layer service

---
+++ src/test/java/com/api/test_backend_java_bbl/service/UserServiceTest.java
@@ -0,0 +1,124 @@
+package com.api.test_backend_java_bbl.service;
+
+import com.api.test_backend_java_bbl.exception.NotFoundException;
+import com.api.test_backend_java_bbl.model.CreateNewUserRequest;
+import com.api.test_backend_java_bbl.model.UpdateUserDetail;
+import com.api.test_backend_java_bbl.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+class UserServiceTest {
+    @Mock
+    private RestTemplate restTemplate;
+
+    @InjectMocks
+    private UserService userService;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+        userService.loadUsers();
+    }
+
+    @Test
+    void getListUser() {
+        List<User> userList = userService.getListUser();
+
+        // assert
+        assertNotNull(userList);
+        assertEquals(10,userList.size());
+    }
+
+    @Test
+    void findByUserId() {
+        long userId = 1L;
+        User user = userService.findByUserId(userId);
+
+        // assert
+        assertNotNull(user);
+        assertEquals("Leanne Graham",user.getName());
+        assertEquals("Bret",user.getUsername());
+        assertEquals("Sincere@april.biz",user.getEmail());
+        assertEquals("1-770-736-8031 x56442",user.getPhone());
+        assertEquals("hildegard.org",user.getWebsite());
+    }
+
+    @Test
+    void findByUserIdShouldThrow() {
+        long userId = 999L;
+        Exception exception = assertThrows(NotFoundException.class,()-> userService.findByUserId(userId));
+
+        // assert
+        assertEquals("User not found",exception.getMessage());
+    }
+
+    @Test
+    void createNewUser() {
+        CreateNewUserRequest mockRequest = new CreateNewUserRequest("name","username","email","phone","website");
+        int total = userService.getListUser().size();
+        User newUser =  userService.createNewUser(mockRequest);
+
+        // assert
+        assertNotNull(newUser);
+        assertEquals("name",newUser.getName());
+        assertEquals("username",newUser.getUsername());
+        assertEquals("email",newUser.getEmail());
+        assertEquals("phone",newUser.getPhone());
+        assertEquals("website",newUser.getWebsite());
+        assertEquals(total+1 ,userService.getListUser().size());
+    }
+
+    @Test
+    void updateUser() {
+        UpdateUserDetail mockRequest = new UpdateUserDetail("name","username","email","phone","website");
+        Long userId = 1L;
+
+        // assert
+        User user = userService.updateUser(userId,mockRequest);
+        assertNotNull(user);
+        assertEquals("name",user.getName());
+        assertEquals("username",user.getUsername());
+        assertEquals("email",user.getEmail());
+        assertEquals("phone",user.getPhone());
+        assertEquals("website",user.getWebsite());
+    }
+
+    @Test
+    void updateUserShouldThrow() {
+        UpdateUserDetail mockRequest = new UpdateUserDetail("name","username","email","phone","website");
+        Long userId = 999L;
+
+        Exception exception = assertThrows(NotFoundException.class,()->userService.updateUser(userId,mockRequest));
+
+        // assert
+        assertEquals("User not found",exception.getMessage());
+    }
+
+    @Test
+    void deleteUser() {
+        Long userId = 1L;
+        int total = userService.getListUser().size();
+
+        userService.deleteUser(userId);
+
+        assertEquals(total - 1 ,userService.getListUser().size());
+    }
+
+    @Test
+    void deleteUserShouldThrow() {
+        Long userId = 999L;
+
+        Exception exception = assertThrows(NotFoundException.class,()->userService.deleteUser(userId));
+
+        assertEquals("User not found" ,exception.getMessage());
+    }
+}
